001b5173457118a8ebcbfbc49860ccee58d5ed8e
unknown <darsinisginfo@gmail.com>
2026-07-09 00:17:13 +0530

    project on music

---
+++ DarsiniVibe.java
@@ -0,0 +1,291 @@
+import java.util.Scanner;
+ 
+class DarsiniVibe
+{
+	public static void main(String args[]){
+		Scanner im = new Scanner(System.in);
+		System.out.println("------------------------------------------------------------");
+		System.out.println("		>> Now Streaming : Darsini's Sonic Era <<	");
+		System.out.println("-------------------------------------------------------------");
+		System.out.println("								");
+		System.out.println("		Choose the  mass artist 		");
+		System.out.println("								");
+		System.out.println("1. Sai Abhyankkar");
+		System.out.println("2. Thalapathy vijay ");
+		System.out.println("3. Shakthisree Gopalan");
+		System.out.println("4. Shreya Goshal");
+	    System.out.println("5. A.R .Rahman ");
+		System.out.println("Sir Yaroda  Music Aura Venum ");
+		int musicchoice = im.nextInt();
+		if(musicchoice == 1){
+			System.out.println("-------------------------------------------------------------------------");
+			System.out.println("		Now Streaming: Sai Abhyankkar's worthy beats 			");
+			System.out.println("--------------------------------------------------------------------------");
+			System.out.println("								");
+			System.out.println("1. Dance hit");
+			System.out.println("2. Melody");
+			System.out.println("3. Slow vibe ");
+            System.out.println("4. Folk Music");
+			System.out.println("5. Rap ");
+			System.out.println("								");
+			System.out.println("Sir enna beat la song venum? ");
+			System.out.println("								");
+ 
+			int musicChoice = im.nextInt();
+			if(musicChoice == 1){
+				System.out.println("------------------------------------------------------------------------- ");
+				System.out.println("  Pavazha Malli ");
+				System.out.println("  Song Lyrics   ");
+				System.out.println(" Ada vittu pona pottu, adi ketti mela paattu…");	
+				System.out.println("-------------------------------------------------------------------------- ");
+			}
+			else if(musicChoice == 2){
+				System.out.println("--------------------------------------------------------------------------- ");
+				System.out.println(" Aasa kooda ");
+				System.out.println(" Song Lyrics ");
+				System.out.println(" Nee pesa lightah aasa kooda vaasam veesum kaatha theda… ");	
+				System.out.println("-------------------------------------------------------------------------- ");
+			}
+			else if(musicChoice == 3){
+				System.out.println("------------------------------------------------------------------------- ");
+				System.out.println(" Vizhi Veekura ");
+				System.out.println(" Song Lyrics ");
+				System.out.println("Vizhi veekura verum vaarthaiyaa, variyaakki nee vaasikkura… ");	
+				System.out.println("-------------------------------------------------------------------------- ");
+			}
+			else if(musicChoice == 4){
+				System.out.println("------------------------------------------------------------------------- ");
+				System.out.println(" Sithira Puthiri ");
+				System.out.println(" Song Lyrics ");
+				System.out.println("Sithira puthiriya kaadhalin thunaiya.. ");	
+				System.out.println("-------------------------------------------------------------------------- ");
+			}
+			else if(musicChoice == 5){
+				System.out.println("------------------------------------------------------------------------- ");
+				System.out.println(" Katchi Sera ");
+				System.out.println(" Song Lyrics ");
+				System.out.println("Sithira puthiriya kaadhalin thunaiya… ");	
+				System.out.println("-------------------------------------------------------------------------- ");
+			}
+			else{
+				System.out.println(" Aura Ve Illa ");
+			}
+		}
+		if(musicchoice == 2){
+			System.out.println("---------------------------------------------------------------------------------");
+			System.out.println("		Now Playing : // Thalapathy vijay- Greatest Of All Time //		");
+			System.out.println("----------------------------------------------------------------------------------");
+			System.out.println("							                          	");
+			System.out.println("1. Dance hit");
+			System.out.println("2. Melody");
+			System.out.println("3. Slow vibe ");
+            System.out.println("4. Folk Music");
+			System.out.println("5. Rap ");
+			System.out.println("								");
+			System.out.println("Sir enna beat la song venum? ");
+			System.out.println("								");
+			int musicChoice = im.nextInt();
+			if(musicChoice == 1){
+				System.out.println("---------------------------------------------------------------------------------- ");
+				System.out.println("Selfie Pulla ");
+				System.out.println(" Song Lyrics");
+				System.out.println(" Hey selfie pulla, kaadhal kaatchi kolla… ");
+				System.out.println("------------------------------------------------------------------------------------ ");
+			}
+			else if(musicChoice == 2){
+				System.out.println("------------------------------------------------------------------------------------- ");
+				System.out.println(" Naa Ready Than ");
+				System.out.println(" Song Lyrics");
+				System.out.println(" Naa ready dhaan varava, thaan ready dhaan varava… ");	
+				System.out.println("----------------------------------------------------------------------------------------");
+			}
+			else if(musicChoice == 3){
+				System.out.println("---------------------------------------------------------------------------------------- ");
+				System.out.println(" Yaar Indha Salai Oram ");
+				System.out.println(" Song Lyrics");
+				System.out.println("Yaar indha saalai oram pookal vaithathu, kaatril engengum vaasam veesuthu… ");
+				System.out.println("----------------------------------------------------------------------------------------- ");
+			}
+			else if(musicChoice == 4){
+				System.out.println("---------------------------------------------------------------------------------------- ");
+				System.out.println(" Asku Laska ");
+				System.out.println(" Song Lyrics");
+				System.out.println("Asku laska, Asku laska... ");
+				System.out.println("----------------------------------------------------------------------------------------- ");
+			}
+			else if(musicChoice == 5){
+				System.out.println("---------------------------------------------------------------------------------------- ");
+				System.out.println(" Aathi");
+				System.out.println(" Song Lyrics");
+				System.out.println("Aathi ena ne paatha udane... ");
+				System.out.println("----------------------------------------------------------------------------------------- ");
+			}
+			
+			else{
+				System.out.println(" Thalapathy Song Ye Illa ");
+			}
+		}
+		if(musicchoice == 3){
+			System.out.println("----------------------------------------------------------------------------------------------");
+			System.out.println("		>> Now Playin Shakthisree Gopalan's Celestial Audio");
+			System.out.println("-----------------------------------------------------------------------------------------------");
+			System.out.println("								");
+			System.out.println("1. Dance hit ");
+			System.out.println("2. Melody ");
+			System.out.println("3. Slow Vibe ");
+		    System.out.println("4. Folk music ");
+			System.out.println("5. Rap ");
+
+			System.out.println("								");
+			System.out.println("Sir Enna beat la song venum ");
+			System.out.println("								");
+ 
+			int musicChoice = im.nextInt();
+			if(musicChoice == 1){
+				System.out.println("---------------------------------------------------------------------------------------------- ");
+				System.out.println(" Yaanji ");
+				System.out.println(" Song Lyrics");
+				System.out.println("yaanji, yaanji , en nenjil vanthu nikura en? ");	
+				System.out.println("---------------------------------------------------------------------------------------------- ");
+			}
+			else if(musicChoice == 2){
+				System.out.println("----------------------------------------------------------------------------------------------- ");
+				System.out.println(" Sir Please Pay the Amount ");
+				System.out.println(" Song Lyrics");
+				System.out.println(" O nenjaathiyae, needhanadi en vaazhkaiyae… ");	
+				System.out.println("----------------------------------------------------------------------------------------------- ");
+			}
+			else if(musicChoice == 3){
+				System.out.println("------------------------------------------------------------------------------------------------ ");
+				System.out.println(" Imaye Imaye ");
+				System.out.println(" Song Lyrics");
+		        System.out.println(" Imaye imaye kaadhal kavidhai, idhayam thodarum un vizhiyin vilai…");
+				System.out.println("--------------------------------- ");
+			}else if(musicChoice == 4){
+				System.out.println("------------------------------------------------------------------------------------------------ ");
+				System.out.println(" Naan Nee ");
+				System.out.println(" Song Lyrics");
+		        System.out.println(" Naan nee naam valave uyire");
+				System.out.println("------------------------------------------------------------------------------------------------ ");
+			
+			}else if(musicChoice == 5){
+				System.out.println("------------------------------------------------------------------------------------------------ ");
+				System.out.println("Railin Oligal");
+				System.out.println(" Song Lyrics");
+		        System.out.println("Railin oligal unaye theduthe athirum parayai idhayam aaduthe ");
+				System.out.println("------------------------------------------------------------------------------------------------- ");
+			}
+			else{
+				System.out.println("Aura Slept  ");
+}}
+	        if(musicchoice == 4){
+			System.out.println("----------------------------------------------------------------------------------------------");
+			System.out.println("		>> Now Playing: Shreya Goshal's Unplugged Version Of Music ");
+			System.out.println("-----------------------------------------------------------------------------------------------");
+			System.out.println("								");
+			System.out.println("1. Dance hit ");
+			System.out.println("2. Melody ");
+			System.out.println("3. Slow Vibe ");
+		    System.out.println("4. Folk music ");
+			System.out.println("5. Rap ");
+
+			System.out.println("								");
+			System.out.println("Sir Enna beat la song venum ");
+			System.out.println("								");
+             int musicChoice = im.nextInt();
+			if(musicChoice == 1){
+				System.out.println("---------------------------------------------------------------------------------------------- ");
+				System.out.println("Mannipaya");
+				System.out.println(" Song Lyrics");
+				System.out.println(" Mannipaya… Mannipaya… ");	
+				System.out.println("---------------------------------------------------------------------------------------------- ");
+			}
+			else if(musicChoice == 2){
+				System.out.println("----------------------------------------------------------------------------------------------- ");
+				System.out.println(" Neethane ");
+				System.out.println(" Song Lyrics");
+				System.out.println(" Neethane neethane en nenjai thattum satham, alagai udaindhen neeye artham… ");	
+				System.out.println("----------------------------------------------------------------------------------------------- ");
+			}
+			else if(musicChoice == 3){
+				System.out.println("------------------------------------------------------------------------------------------------ ");
+				System.out.println(" Velicha poove ");
+				System.out.println(" Song Lyrics");
+		        System.out.println(" Vaa vaa en velicha poove vaa, Uyir theetum uyile vaa, kulir neekum veilye vaa…");
+				System.out.println("--------------------------------------------------------------------------------------------------- ");
+			}else if(musicChoice == 4){
+				System.out.println("------------------------------------------------------------------------------------------------ ");
+				System.out.println(" Pidikuthe");
+				System.out.println(" Song Lyrics");
+		        System.out.println(" Pidikuthe thirumba thirumba unnai");
+				System.out.println("------------------------------------------------------------------------------------------------ ");
+			}else if(musicChoice == 5){
+				System.out.println("------------------------------------------------------------------------------------------------ ");
+				System.out.println("Anbae Peranbae");
+				System.out.println(" Song Lyrics");
+		        System.out.println("yeno iravodu oliyaai koodum uravondru ketkiren, varaimeerum ivalin aasai niraivera paarkiren ");
+				System.out.println("------------------------------------------------------------------------------------------------- ");
+			}
+			else{
+				System.out.println("Vibe Mudinchu  ");
+			}
+		}
+	
+
+          if(musicchoice==5){
+			System.out.println("----------------------------------------------------------------------------------------------");
+			System.out.println("		>> Now Playing : Isai Puyal A.R. Rahman's rocking songs  ");
+			System.out.println("-----------------------------------------------------------------------------------------------");
+			System.out.println("								");
+			System.out.println("1. Dance hit ");
+			System.out.println("2. Melody ");
+			System.out.println("3. Slow Vibe ");
+		    System.out.println("4. Folk music ");
+			System.out.println("5. Rap ");
+
+			System.out.println("								");
+			System.out.println("Sir Enna beat la song venum ");
+			System.out.println("								");
+ 
+			int musicChoice = im.nextInt();
+			if(musicChoice == 1){
+				System.out.println("---------------------------------------------------------------------------------------------- ");
+				System.out.println("Aaruyire ");
+				System.out.println(" Song Lyrics");
+				System.out.println("Aaruyire manipaya? Aaruyire manipaya? solladi, en sakiye  ");	
+				System.out.println("---------------------------------------------------------------------------------------------- ");
+			}
+			else if(musicChoice == 2){
+				System.out.println("----------------------------------------------------------------------------------------------- ");
+				System.out.println(" Hossana ");
+				System.out.println(" Song Lyrics");
+				System.out.println("Antha neram andhi  neram kan parthu kandhalagi pona neram etho aache  ");	
+				System.out.println("----------------------------------------------------------------------------------------------- ");
+			}
+			else if(musicChoice == 3){
+				System.out.println("------------------------------------------------------------------------------------------------ ");
+				System.out.println("Naan Un ");
+				System.out.println(" Song Lyrics");
+		        System.out.println("Naan un alaginile, dheivam unargiren …");
+				System.out.println("--------------------------------- ");
+			}else if(musicChoice == 4){
+				System.out.println("------------------------------------------------------------------------------------------------ ");
+				System.out.println("Muzhumathi ");
+				System.out.println(" Song Lyrics");
+		        System.out.println(" Muzhumathi avalathu mugamagum , malligai avaluthu manammagum");
+				System.out.println("------------------------------------------------------------------------------------------------ ");
+			}else if(musicChoice == 5){
+				System.out.println("------------------------------------------------------------------------------------------------ ");
+				System.out.println("Aga naga");
+				System.out.println(" Song Lyrics");
+		        System.out.println("Aga naga, aga naga muga nagaye ohhhh, muga naga muga naga muru nagayee ohhh");
+				System.out.println("------------------------------------------------------------------------------------------------- ");
+			}
+			else{
+				System.out.println("Rahman tired agitaru ");
+			}
+		}
+	}
+}
+
+	
