6a9972e7f6ef04de34c89e8814428de00b7daddd
unknown <darsinisginfo@gmail.com>
2026-07-09 18:50:02 +0530

    loopps

---
+++ Loopps.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Loopps
+ {
+    public static void main(String[] args)
+	{
+        
+        // Loop starts at 1 and runs until it reaches 10
+        for (int i = 1; i <= 10; i++)
+			{
+            System.out.println(i);
+        }
+        
+    }
+ }
