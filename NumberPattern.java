fcc223a55815736f65b773b1d24de4454680d90a
unknown <darsinisginfo@gmail.com>
2026-07-10 20:59:44 +0530

    Number pattern

---
+++ NumberPattern.java
@@ -0,0 +1,21 @@
+public class NumberPattern
+ {
+    public static void main(String[] args)
+	{
+        int rows = 5;
+
+        // Outer loop for rows
+        for (int i = 1; i <= rows; i++)
+			{
+            
+            // Inner loop
+            for (int j = 1; j <= i; j++)
+				{
+                System.out.print(j);
+            }
+            
+            // Move to the next line
+            System.out.println();
+        }
+    }
+}
