e0e889277ed7bce0fe19a2873f2047b27151c780
unknown <darsinisginfo@gmail.com>
2026-07-14 20:23:06 +0530

    Even odd

---
+++ Evenodd.java
@@ -0,0 +1,21 @@
+
+
+class Evenodd {
+    public static void main(String[] args) {
+        // Create a Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        // Check if the remainder is 0 when divided by 2
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+        
+        // Close the scanner to avoid memory leaks
+        scanner.close();
+    }
+}
