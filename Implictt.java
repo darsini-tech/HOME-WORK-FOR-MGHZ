8e2d281d20d8902f78943256ae483d57ca207168
unknown <darsinisginfo@gmail.com>
2026-07-17 21:26:45 +0530

    Character to integer type casting

---
+++ Implictt.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Implictt {
+ 
+    public static void main(String[] args)
+	{
+        Scanner scanner = new Scanner(System.in);
+        
+        //  character input
+        char charValue = scanner.next().charAt(0);
+        
+        
+        int asciiValue = charValue;
+        
+        
+        System.out.println(asciiValue);
+    }
+}
